86addd1d9fae7fb9a8cada409024d7c2c93b375e
Angela May Xie <amx@utexas.edu>
2016-03-10 22:28:16 -0600

    styled headers using text-transform, slightly better 404 page, not sure if i like the color change but oh well

--- angela.js
+++ angela.js
@@ -8,23 +8,30 @@ function procede() {
 
     $(".hovertitle").hover(
         function() {//mouse in
-            colorChange(this);
+            hoverColorChange(this);
         }, function() {//mouse out
             $(this).css("color", "white");
         }
     );
-
+    
+    hoverlessColorChange($('#404'));
 }
 
-function colorChange(element) {
+function hoverColorChange(element) {
     if(!$(element).is(":hover")) {
         $(this).css("color","white");
     } else {
         var rand = Math.floor(Math.random()*16777215).toString(16);
         $(element).css("color", '#' + rand);
 
-        setTimeout(function(){ colorChange(element);}, 1000);
+        setTimeout(function(){ hoverColorChange(element);}, 1000);
     }
+}
+
+function hoverlessColorChange(element) {
+    var rand = Math.floor(Math.random()*16777215).toString(16);
+    $(element).css("color", '#' + rand);
+    setTimeout(function(){ hoverlessColorChange(element);}, 1000);    
 }
 
 function showMenu() {
